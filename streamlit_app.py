3794951a7012189d77c57efdd5a5f2d49ed047b6
jayuan101 <43624484+jayuan101@users.noreply.github.com>
2024-06-13 11:07:21 -0400

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
+pip install scikit-learn
+
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error, accuracy_score
-from sklearn.model_selection import train_test_split
+
 # Load the CSV file
 file_path = 'salaries.csv'  # Use forward slashes
 df = pd.read_csv("salaries.csv")
