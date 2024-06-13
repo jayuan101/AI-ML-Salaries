89682ef6f26a42a7e12495d83e8944658c3b740b
jayuan101 <43624484+jayuan101@users.noreply.github.com>
2024-06-13 00:21:23 -0400

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,36 @@
 import streamlit as st
 import pandas as pd
 
-# Load the CSV file
-file_path = 'Users/young/Downloads/salaries.csv'
-df = pd.read_csv("salaries.csv")
-
 # Streamlit app
 def main():
     st.title("Salaries Data Dashboard")
 
-    # Display the DataFrame
-    st.write("Different Salaries")
-    st.write(df)
+    # Upload CSV file
+    uploaded_file = st.file_uploader("salaries.csv", type="csv")
+    if uploaded_file is not None:
+        # Read the CSV file
+        df = pd.read_csv(uploaded_file)
 
-    # Filter by job title
-    job_titles = df['job_title'].unique()
-    selected_job_title = st.selectbox("Select Job Title", job_titles)
-    filtered_df = df[df['job_title'] == selected_job_title]
-    st.write(f"## Data for {selected_job_title}")
-    st.write(filtered_df)
+        # Display the DataFrame
+        st.write("## Data")
+        st.write(df)
 
-    # Display basic statistics
-    st.write("## Summary Statistics")
-    st.write(filtered_df.describe())
+        # Filter by job title
+        job_titles = df['job_title'].unique()
+        selected_job_title = st.selectbox("Select Job Title", job_titles)
+        filtered_df = df[df['job_title'] == selected_job_title]
+        st.write(f"## Data for {selected_job_title}")
+        st.write(filtered_df)
 
-    # Display a line chart
-    st.write("## Salary Distribution")
-    st.line_chart(filtered_df['salary_in_usd'])
+        # Display basic statistics
+        st.write("## Summary Statistics")
+        st.write(filtered_df.describe())
+
+        # Display a line chart
+        st.write("## Salary Distribution")
+        st.line_chart(filtered_df['salary_in_usd'])
+    else:
+        st.write("Please upload a CSV file to proceed.")
 
 if __name__ == "__main__":
     main()
